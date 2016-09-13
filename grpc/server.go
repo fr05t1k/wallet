b56cc940c0ed27ed5e84e47fcd85ad57e55d9140
Stanislav Pavlovichev <pavlovichev@coccoc.com>
2016-09-13 23:55:31 +0700

    move serve to another package

---
+++ grpc/server.go
@@ -0,0 +1,34 @@
+package wallet
+
+import (
+	"net"
+	"google.golang.org/grpc"
+	"fmt"
+	"log"
+	"golang.org/x/net/context"
+	"github.com/fr05t1k/wallet/wallet"
+)
+
+type WalletServer struct {
+}
+
+func (server *WalletServer)GetTotal(ctx context.Context, total *wallet.Total) (*wallet.Total, error) {
+	return total, nil
+}
+
+func (server *WalletServer)Run(port uint16) {
+	log.Println("Starting GRPC server")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		panic("Can't run grpc server")
+	}
+	grpcServer := grpc.NewServer()
+
+	wallet.RegisterWalletServer(grpcServer, &WalletServer{})
+	log.Println(fmt.Sprintf("GRPC server listening on: :%d", port))
+	err = grpcServer.Serve(lis)
+	log.Fatalf("Failed start GRPC server %v", err)
+	panic("Failed start GRPC server")
+}
+
--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"os"
-	"gopkg.in/mgo.v2"
 	_ "github.com/joho/godotenv"
 	"github.com/fr05t1k/wallet/db"
-	"github.com/fr05t1k/wallet/wallet"
 	"strconv"
 )
+import server "github.com/fr05t1k/wallet/grpc"
 
 var (
 	port, _ = strconv.Atoi(os.Getenv("WALLET_PORT"))
@@ -15,10 +14,9 @@ var (
 	mongoDBName = os.Getenv("WALLET_MONGODB_DB_NAME")
 )
 
-
 func init() {
 	db.Connect(mongoConnectionString, mongoDBName)
-	runner := &wallet.WalletServerRunner{}
+	runner := &server.WalletServer{}
 
 	runner.Run(uint16(port))
 }
